933757706696e95baf96c94022a8cd036946b5e8
Michael <c14180035@john.petra.ac.id>
2021-11-07 17:16:47 +0700

    no message

---
+++ Assets/Code/YellowBird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBird : Bird
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float _boostForce = 100;
+    public bool _hasBoost = false;
+
+    public void Boost()
+    {
+        if (State == BirdState.Thrown && !_hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            _hasBoost = true;
+        }
+    }
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnTap()
+    {
+        Boost();
+        Debug.Log("Tap");
+    }
+}
